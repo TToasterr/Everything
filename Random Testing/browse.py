4ead6f786ca1749b800add50a270a769b95357e5
Toaster <ttoasterrr@gmail.com>
2018-09-08 15:58:25 -0600

    Removed unecesarry bits
    import os, import time, and time.sleep were removed.

--- Random Testing/browse.py
+++ Random Testing/browse.py
@@ -1,10 +1,7 @@
 import webbrowser
-import os
-import time
 
 new = 2
 url = "https://www.reddit.com"
 
 webbrowser.open(url,new=new)
-time.sleep(5)
-#cant close browser stuff ;(    
+#cant close browser stuff ;(
