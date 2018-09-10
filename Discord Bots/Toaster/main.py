5ce7b48017ea29b929a7d53926b884d676af4740
Toaster <ttoasterrr@gmail.com>
2018-09-10 06:29:33 -0600

    Added -help and -annoyJonathan

--- Discord Bots/Toaster/main.py
+++ Discord Bots/Toaster/main.py
@@ -1,6 +1,8 @@
 import discord
+import random
 
 client = discord.Client()
+help = "-help - shows this \n-ping - pings the bot \n-whoami - gives users name and account creation date \n-annoyJonathan [amount of times] - pings jonathan the amount of times specified. Because of Discord spam limits, sends messages in bursts of 5."
 
 
 
@@ -15,11 +17,22 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith("-help"):
+        await client.send_message(message.channel, content = help)
+
     if message.content.startswith("-ping"):
         await client.send_message(message.channel, content = "Pong!")
 
     if message.content.startswith("-whoami"):
         await client.send_message(message.channel, content = "You're **%s**! \nYou've been on discord since `%s`" % (message.author, message.author.created_at))
+
+    if message.content.startswith("-annoyJonathan"):
+        howmany = int(message.content[14:])
+        jonathan = "<@136664437431074816>"
+        msgNum = 1
+        for i in range(howmany):
+            await client.send_message(message.channel, content = "%s: %s" % (msgNum, jonathan))
+            msgNum += 1
 
 
 
