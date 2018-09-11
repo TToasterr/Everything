43787f1cb60dfc187cfee13fc1fafd7fb2ae87e3
Toaster <38060892+TToasterr@users.noreply.github.com>
2018-09-11 13:18:22 -0600

    Created words.py

---
+++ Random Testing/1200- Accurate Human Simulator/words.py
@@ -0,0 +1,49 @@
+startingNouns = [
+    "Donald Trump", "A disabled man", "The White House", "Government", "Spaghetti", "A chef", "An abusive father", "A person", "Logan Paul", "A gene edited cat girl", "An autistic person", "A furry", "A hidden android", "A failed abortion", "A Hiroshima survivor", "The nuke that hit Japan", "A Florida man", "A potato", "Some fric", "23 year old abortion, Logan Paul,", "Anime", "A robot", "An asshole", "A rug", "A desk", "A gaming PC", "A condom", "A twenty dollar bill", "A goldfish", "A shark", "An ocean", "Apple", "A high school", "A middle school", "A tissue box", "A sink", "A window", "A laptop", "A butterfly knife", "A knife", "A text editor", "A code", "An AI", "A sentient robot", "A secretly sentient robot", "A t-shirt", "An emo kid", "A dragon", "A weed whacker", "A bat-shit crazy Amish person", "A pervert", "An accountant", "Steven hawking", "A plant", "A pet plant", "A pet robot", "A cat", "A pet cat", "Stefan Karl Stefanson", "A liberal", "A millenial", "A middle schooler", "A high schooler", "A sCeCRet FUC"
+]
+
+nouns = [
+    "disabled man", "Donald Trump", "White House", "Government", "spaghetti", "chef", "abusive father", "person", "Logan Paul", "gene edited cat girl", "autistic person", "furry", "hidden android", "failed abortion", "Hiroshima survivor", "nuke that hit Japan", "Florida man", "potato", "fric", "23 year old abortion, Logan Paul,", "anime", "robot", "asshole", "rug", "desk", "gaming PC", "condom", "twenty dollar bill", "goldfish", "shark", "ocean", "Apple", "high school", "middle school", "tissue box", "sink", "window", "laptop", "butterfly knife", "knife", "text editor", "code", "AI", "sentient robot", "secretly sentient robot", "t-shirt", "emo kid", "dragon", "weed whacker", "bat-shit crazy Amish person", "pervert", "accountant", "Steven hawking", "plant", "pet plant", "pet robot", "cat", "pet cat", "Stefan Karl Stefanson", "liberal", "millenial", "middle schooler", "high schooler", "sCeCRET FUC"
+]
+
+adjectives = [
+    "fucking", "terrible", "masculine", "amish", "christian", "short", "Jonathan-sized", "retarded", "suicidal", "depressed", "drug-addicted", "beaten-up", "run-down", "furry version of a/an", "dead", "hairless", "sadistic", "out of control", "sexy", "naked", "unholy", "massive", "drunken", "arrogant", "fucking crazy", "mentally impaired", "disabled", "wonky", "gene edited", "spooky", "spicy", "cloned", "public", "loli", "new", "long", "wide", "fucking", "lil", "sterile", "autistic", "spooky", "leftist", "liberal", "millenial", "'wahoo' screaming"
+]
+
+endingVerbs = [
+    "dies", "nuts", "sucks", "kills themself", "jumps off a bridge", "dabs into oblivion", "ties a noose", "commits sewer slide", "is ass raped", "shrinks in the washer", "gets laid", "gets its period", "yeets so hard they fucking die", "is penetrated", "commits die"
+]
+
+verbs = [
+    "stabs", "sucks off", "kills", "creates", "summons", "murders", "massacres", "roasts", "pokes", "slaps", "snaps the neck of", "interrogates", "dates", "ties a noose for", "friendzones", "medicates", "explodes", "abolishes", "fucks", "penetrates"
+]
+
+adverbs = [
+    "slowly", "zestily", "sexily", "fortunately", "daily", ", every tuesday,", "almost", "blindly", "madly", "speedily", "viciously", "yearly", ", every saturday,", "monthly", "angrily", "juicily"
+]
+
+#------------------------------------------------
+
+FFstartingNouns = [
+    "Donald Trump", "An elderly man", "The White House", "Government", "Spaghetti", "A chef", "A father", "A person", "Logan Paul", "A gene edited cat girl", "A person", "A furry", "A hidden android", "A Hiroshima survivor", "A Florida man", "A potato", "Anime", "A robot", "A rug", "A desk", "A gaming PC", "A twenty dollar bill", "A goldfish", "A shark", "An ocean", "Apple", "A high school", "A middle school", "A tissue box", "A sink", "A window", "A laptop", "A butterfly knife", "A knife", "A text editor", "A code", "An AI", "A sentient robot", "A secretly sentient robot", "A t-shirt", "An emo kid", "A dragon", "A weed whacker", "An accountant", "Steven hawking", "A plant", "A pet plant", "A pet robot", "A cat", "A pet cat", "Stefan Karl Stefanson", "A liberal", "A millenial", "A high schooler", "A middle schooler"
+]
+
+FFnouns = [
+    "elderly man", "Donald Trump", "White House", "Government", "spaghetti", "chef", "father", "person", "Logan Paul", "gene edited cat girl", "furry", "hidden android", "Hiroshima survivor", "Florida man", "potato", "fric", "anime", "robot", "rug", "desk", "gaming PC", "twenty dollar bill", "goldfish", "shark", "ocean", "Apple", "high school", "middle school", "tissue box", "sink", "window", "laptop", "butterfly knife", "knife", "text editor", "code", "AI", "sentient robot", "secretly sentient robot", "t-shirt", "emo kid", "dragon", "weed whacker", "crazy Amish person", "accountant", "Steven hawking", "plant", "pet plant", "pet robot", "cat", "pet cat", "Stefan Karl Stefanson", "liberal", "millenial", "middle schooler", "high schooler"
+]
+
+FFadjectives = [
+    "random", "terrible", "masculine", "amish", "christian", "short", "Jonathan-sized", "depressed", "out of touch", "beaten-up", "run-down", "furry version of a/an", "dead", "hairless", "sadistic", "out of control", "memey", "stupid", "unholy", "massive", "drunken", "arrogant", "'wahoo' screaming", "mentally impaired", "disabled", "wonky", "gene edited", "spooky", "spicy", "cloned", "public", "new", "long", "wide", "lil", "sterile", "spooky", "leftist", "liberal", "millenial"
+]
+
+FFendingVerbs = [
+    "dies", "sucks", "dabs into oblivion", "commits sewer slide", "shrinks in the washer", "yeets so hard they absolutely die", "commits die", "is poked", "is killed", "is created", "is summoned", "is roasted", "is friendzoned", "is medicated", "is exploded"
+]
+
+FFverbs = [
+    "pokes", "kills", "creates", "summons", "murders", "massacres", "roasts", "pokes", "slaps", "interrogates", "dates", "friendzones", "medicates", "explodes", "abolishes", "draws","yeets"
+]
+
+FFadverbs = [
+    "slowly", "zestily", "fortunately", "daily", ", every tuesday,", "almost", "blindly", "madly", "speedily", "viciously", "yearly", ", every saturday,", "monthly", "angrily", "juicily"
+]
