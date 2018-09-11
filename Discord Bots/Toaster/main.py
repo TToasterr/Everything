94d7249a2a7f5c9d46a0539ff45efc866d120236
Toaster <ttoasterrr@gmail.com>
2018-09-10 23:04:31 -0600

    LOTS of stuff. Mainly -memes

--- Discord Bots/Toaster/main.py
+++ Discord Bots/Toaster/main.py
@@ -1,9 +1,14 @@
 import discord
+import praw
+import pandas as pd
 import random
 import time
+import sys
+sys.path.append("C:/Users/matth/Desktop/Everything")
+import redditStuff as rs
 
 client = discord.Client()
-help = "**-help** - shows this \n**-ping** - pings the bot \n**-whoami** - gives users name and account creation date \n**-jonathan, [time interval], [amount of times], [message]** - pings jonathan the amount of times specified. Because of Discord spam limits, sends messages in bursts of 5. \n**-poll [message]** - creates a poll."
+help = "**-help** - shows this \n**-ping** - pings the bot \n**-invite** - gives an invite link so you can add the bot to more servers. \n**-jonathan, [time interval], [amount of times], [message]** - pings jonathan the amount of times specified. Because of Discord spam limits, sends messages in bursts of 5. \n**-poll [message]** - creates a poll. \n**-meme** - pulls a random image from r/dankMemes or r/memes. There is some delay. \n**-susquote** - pulls a random image from r/suspiciousquotes. There is some delay."
 
 
 
@@ -20,12 +25,15 @@ async def on_message(message):
 
     if message.content.startswith("-help"):
         await client.send_message(message.channel, content = help)
+        print("%s asked for help." % message.author)
 
     if message.content.startswith("-ping"):
         await client.send_message(message.channel, content = "Pong!")
+        print("%s pinged the bot." % message.author)
 
-    if message.content.startswith("-whoami"):
-        await client.send_message(message.channel, content = "You're **%s**! \nYou've been on discord since `%s`" % (message.author, message.author.created_at))
+    if message.content.startswith("-invite"):
+        await client.send_message(message.channel, content = "Invite me with this link! \nhttps://discordapp.com/oauth2/authorize?client_id=488570938581975041&scope=bot")
+        print("%s is possibly inviting the bot to a new server!" % message.author)
 
     if message.content.startswith("-jonathan"):
         args = message.content.split(", ")
@@ -33,14 +41,14 @@ async def on_message(message):
         try:
             a = args[3]
         except:
-            print("A user tried to ping jonathan but didn't input enough arguments!")
+            print("%s tried to ping jonathan but didn't input enough arguments!" % message.author)
             await client.send_message(message.channel, content = "You didn't use enough arguments! Do -help for help, you **VERY FINE MEMBER OF THE LBGT COMMUNITY**.")
             return
 
         try:
             interval = float(args[1])
         except:
-            print("A user tried to ping jonathan but didnt use a number as the interval!")
+            print("%s tried to ping jonathan but didnt use a number as the interval!" % message.author)
             if args[1] == "":
                 args[1] = None
             await client.send_message(message.channel, content = "%s isn't a number! Try again you **RATHER LARGE GAY CHILD**." % args[1])
@@ -49,7 +57,7 @@ async def on_message(message):
         try:
             amount = int(args[2])
         except:
-            print("A user tried to ping jonathan but didnt use a number as the amount!")
+            print("%s tried to ping jonathan but didnt use a number as the amount!" % message.author)
             if args[2] == "":
                 args[2] = None
             await client.send_message(message.channel, content = "%s isn't a number! Try again you **REALLY REALLY NOT STRAIGHT MEMBER OF THE HUMAN SPECIES**." % args[2])
@@ -60,12 +68,12 @@ async def on_message(message):
         lowLimit = 1
 
         if amount > highLimit:
-            print("A user tried to ping jonathan but used too high of an amount.")
+            print("%s tried to ping jonathan but used too high of an amount." % message.author)
             await client.send_message(message.channel, content = "You can't send more than %s messages! Nice try you **LARGE HOMOSEXUAL**." % highLimit)
             return
 
         if amount < lowLimit:
-            print("A user tried to ping jonathan but used too low of an amount.")
+            print("%s tried to ping jonathan but used too low of an amount." % message.author)
             await client.send_message(message.channel, content = "You can't send a negative (or zero) amount of messages! Nice try you **LARGE HOMOSAPIEN**.")
             return
 
@@ -78,6 +86,8 @@ async def on_message(message):
     if message.content.startswith("-poll"):
         await client.add_reaction(message, "👍")
         await client.add_reaction(message, "👎")
+
+        print("%s called a poll." % message.author)
 
     if message.content.startswith("-streaming"):
 
@@ -101,8 +111,42 @@ async def on_message(message):
             await client.send_message("451206133810724864", content = "Toaster is live, playing %s! Check it out: \nhttps://www.twitch.tv/ttoasterrr")
 
         else:
-            print("Someone tried to start a stream without the 'Toaster' role.")
+            print("%s tried to start a stream without the 'Toaster' role." % message.author)
             await client.send_message(message.channel, content = "You don't have the correct role to do this!")
+
+    if message.content.startswith("-meme"):
+        rand1 = random.randint(0,1)
+        if rand1 == 0:
+            subreddit = rs.reddit.subreddit('dankMemes')
+        else:
+            subreddit = rs.reddit.subreddit('memes')
+
+        top_subreddit = subreddit.hot(limit = 200)
+        posts = []
+
+        for submission in top_subreddit:
+            posts.append(submission.url)
+
+        rand1 = random.randint(0,len(posts)-1)
+        randpost = posts[rand1]
+
+        await client.send_message(message.channel, content = randpost)
+        print("%s called a meme." % message.author)
+
+    if message.content.startswith("-susquote"):
+        subreddit = rs.reddit.subreddit('suspiciousquotes')
+
+        top_subreddit = subreddit.top(limit = 200)
+        posts = []
+
+        for submission in top_subreddit:
+            posts.append(submission.url)
+
+        rand1 = random.randint(0,len(posts)-1)
+        randpost = posts[rand1]
+
+        await client.send_message(message.channel, content = randpost)
+        print("%s called a suspicious quote." % message.author)
 
 
 
