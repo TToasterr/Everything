5fc90f89d1ad433ee4bae6c759a9ec52f1a60d4c
Toaster <ttoasterrr@gmail.com>
2018-09-15 23:55:43 -0600

    Added power-ups and made them function

--- Random Testing/Python/Dungeon Game/game.py
+++ Random Testing/Python/Dungeon Game/game.py
@@ -27,18 +27,18 @@ boards = [
         ["█","□","░","░","░","░","░","█","█","█"],
         ["█","█","█","█","█","█","█","█","█","█"]
     ],
-      [
-          ["█","█","█","█","█","█","█","█","█","█"],
-          ["█","█","█","░","░","░","░","█","█","█"],
-          ["█","█","░","░","░","░","░","░","█","█"],
-          ["█","█","█","░","░","█","█","░","░","█"],
-          ["█","█","□","░","░","█","□","░","░","█"],
-          ["█","█","█","█","░","░","░","░","█","█"],
-          ["█","█","█","█","█","░","░","░","█","█"],
-          ["█","█","█","░","░","░","█","█","█","█"],
-          ["█","□","░","░","░","█","█","█","█","█"],
-          ["█","█","█","█","█","█","█","█","█","█"]
-      ]
+    [
+        ["█","█","█","█","█","█","█","█","█","█"],
+        ["█","█","█","░","░","░","░","█","█","█"],
+        ["█","█","░","░","░","░","░","░","█","█"],
+        ["█","█","█","░","░","█","█","░","░","█"],
+        ["█","█","□","░","░","█","□","░","░","█"],
+        ["█","█","█","█","░","░","░","░","█","█"],
+        ["█","█","█","█","█","░","░","░","█","█"],
+        ["█","█","█","░","░","░","█","█","█","█"],
+        ["█","□","░","░","░","█","█","█","█","█"],
+        ["█","█","█","█","█","█","█","█","█","█"]
+    ]
 ]
 monsterCount = [4,4,3]
 tile = 0
@@ -48,8 +48,17 @@ wall = "█"
 floor = "░"
 player = "■"
 monster = "□"
-types = ["Gremlin","Orc","Skeleton","Toaster"]
-health = 200
+hpUpItem = "▲"
+attUpItem = "●"
+key = "%s: wall \n%s: floor \n%s: player \n%s: monster \n%s: health up \n%s: attack up \n" % (wall,floor,player,monster,hpUpItem,attUpItem)
+types = ["Gremlin","Orc","Skeleton","Toaster","Zombie","Crazed Miner"]
+healthMax = [200, 250, 300, 350, 400, 450]
+attMax = [200, 250, 300, 400, 600, 900]
+attUp = 0
+hpUpp = 0
+enHpMax = [500, 600, 750]
+enAttMax = [75, 100, 150]
+health = healthMax[hpUpp]
 
 
 
@@ -59,6 +68,10 @@ health = 200
 
 def printBoard():
     global boards
+    global key
+    global stats
+    print(key)
+    print(stats)
     for i in boards[tile]:
         print(*i, sep="  ")
 
@@ -77,10 +90,72 @@ def checkMCount():
 
     if monsterCount[tile] == 0:
         tile += 1
+        powerUpf()
         if tile == 1:
             playerPos = [7,5]
         if tile == 2:
             playerPos = [2,4]
+
+
+
+def powerUpf():
+    global hpUpItem
+    global attUpItem
+    global wall
+    global coordsX
+    global coordsY
+    global powerUp
+    global powerUpType
+
+    powerUp = random.randint(0,1)
+    if powerUp == 0:
+        powerUp = False
+        print("You don't get a power up! \n")
+    else:
+        print("You got a power up! \n")
+        powerUpType = random.randint(0,1)
+        if powerUpType == 0:
+            powerUpType = hpUpItem
+        else:
+            powerUpType = attUpItem
+        powerUpPlace()
+
+def powerUpPlace():
+    global powerUp
+    global coordsX
+    global coordsY
+    global powerUpType
+    global boards
+
+    if powerUp:
+        powerUpCoords()
+        if boards[tile][coordsX][coordsY] == wall or boards[tile][coordsX][coordsY] == monster or boards[tile][coordsX][coordsY] == player:
+            powerUpPlace()
+
+        else:
+            boards[tile][coordsX][coordsY] = powerUpType
+
+
+
+def powerUpCoords():
+    global coordsX
+    global coordsY
+    coordsX = random.randint(0,9)
+    coordsY = random.randint(0,9)
+
+
+
+def attackUp():
+    global attMax
+    global attUp
+    attUp += 1
+
+
+
+def hpUp():
+    global healthMax
+    global hpUpp
+    hpUpp += 1
 
 
 
@@ -97,10 +172,12 @@ def fightInput():
 def getStats():
     global enType
     global enHealth
+    global enHpMax
+    global tile
 
     enType = random.randint(0,len(types)-1)
     enType = types[enType]
-    enHealth = random.randint(50,500)
+    enHealth = random.randint(50,enHpMax[tile])
 
 
 
@@ -108,7 +185,6 @@ def printFight():
     global enType
     global enHealth
     global health
-
     print("ENEMY TYPE: \n%s \n\nENEMY HEALTH: \n%s \n\n\n\nYOUR HEALTH: \n%s \n" % (enType, enHealth, health))
 
 
@@ -118,6 +194,8 @@ def checkHealths():
     global enHealth
     global tile
     global monsterCount
+    global healthMax
+    global hpUpp
 
     if health <= 0:
         print(a)
@@ -125,7 +203,7 @@ def checkHealths():
         sys.exit()
 
     if enHealth <= 0:
-        health = 200
+        health = healthMax[hpUpp]
         monsterCount[tile] -= 1
         print(a)
         print("You killed the monster, and were healed to full health. \n")
@@ -138,6 +216,11 @@ def fight():
     global a
     global health
     global enHealth
+    global AttMax
+    global enAttMax
+    global tile
+    global attUp
+    global hpUpp
 
     printFight()
     checkHealths()
@@ -148,23 +231,23 @@ def fight():
         print("Input 'attack' or 'heal' to attack or heal. \n")
         fight()
 
-    if finp == "attack":
-        dmgd = random.randint(1,200)
+    if finp == "attack" or finp == "a":
+        dmgd = random.randint(1,attMax[attUp])
         enHealth -= dmgd
-        dmgt = random.randint(1,75)
+        dmgt = random.randint(1,enAttMax[attUp])
         health -= dmgt
         print(a)
         print("You dealt %s damage. \n\nThe monster hit back, and dealt %s damage. \n" % (dmgd, dmgt))
         fight()
 
-    if finp == "heal":
-        dmgt = random.randint(1,75)
+    if finp == "heal" or finp == "h":
+        dmgt = random.randint(1,enAttMax[attUp])
         health -= dmgt
         healAmm = random.randint(0,100)
         health += healAmm
 
-        if health >= 200:
-            health = 200
+        if health >= healthMax[hpUpp]:
+            health = healthMax[hpUpp]
             print(a)
             print("You were already at, or healed to, full health! \n\nThe monster hit you while you were healing, and dealt %s damage. \n" % dmgt)
         else:
@@ -193,7 +276,11 @@ def yoinks():
     global boards
     global a
     global health
+    global hpUpItem
+    global attUpItem
+    global stats
 
+    stats = "HEALTH MAX: %s \nATTACK MAX: %s" % (healthMax[hpUpp],attMax[attUp])
     checkMCount()
     boards[tile][playerPos[0]][playerPos[1]] = player
     printBoard()
@@ -211,7 +298,28 @@ def yoinks():
             yoinks()
             return
 
+        elif boards[tile][playerPos[0]-1][playerPos[1]] == hpUpItem:
+            boards[tile][playerPos[0]][playerPos[1]] = floor
+            playerPos[0] -= 1
+            boards[tile][playerPos[0]][playerPos[1]] = player
+            print(a)
+            print("You got an HP up! \n")
+            hpUp()
+            yoinks()
+            return
+
+        elif boards[tile][playerPos[0]-1][playerPos[1]] == attUpItem:
+            boards[tile][playerPos[0]][playerPos[1]] = floor
+            playerPos[0] -= 1
+            boards[tile][playerPos[0]][playerPos[1]] = player
+            print(a)
+            print("You got an ATTACK up! \n")
+            attackUp()
+            yoinks()
+            return
+
         elif boards[tile][playerPos[0]-1][playerPos[1]] == monster:
+            health = healthMax[hpUpp]
             boards[tile][playerPos[0]][playerPos[1]] = floor
             playerPos[0] -= 1
             boards[tile][playerPos[0]][playerPos[1]] = player
@@ -235,7 +343,28 @@ def yoinks():
             yoinks()
             return
 
+        elif boards[tile][playerPos[0]+1][playerPos[1]] == hpUpItem:
+            boards[tile][playerPos[0]][playerPos[1]] = floor
+            playerPos[0] += 1
+            boards[tile][playerPos[0]][playerPos[1]] = player
+            print(a)
+            print("You got an HP up! \n")
+            hpUp()
+            yoinks()
+            return
+
+        elif boards[tile][playerPos[0]+1][playerPos[1]] == attUpItem:
+            boards[tile][playerPos[0]][playerPos[1]] = floor
+            playerPos[0] += 1
+            boards[tile][playerPos[0]][playerPos[1]] = player
+            print(a)
+            print("You got an ATTACK up! \n")
+            attackUp()
+            yoinks()
+            return
+
         elif boards[tile][playerPos[0]+1][playerPos[1]] == monster:
+            health = healthMax[hpUpp]
             boards[tile][playerPos[0]][playerPos[1]] = floor
             playerPos[0] += 1
             boards[tile][playerPos[0]][playerPos[1]] = player
@@ -259,7 +388,28 @@ def yoinks():
             yoinks()
             return
 
+        elif boards[tile][playerPos[0]][playerPos[1]-1] == hpUpItem:
+            boards[tile][playerPos[0]][playerPos[1]] = floor
+            playerPos[1] -= 1
+            boards[tile][playerPos[0]][playerPos[1]] = player
+            print(a)
+            print("You got an HP up! \n")
+            hpUp()
+            yoinks()
+            return
+
+        elif boards[tile][playerPos[0]][playerPos[1]-1] == attUpItem:
+            boards[tile][playerPos[0]][playerPos[1]] = floor
+            playerPos[1] -= 1
+            boards[tile][playerPos[0]][playerPos[1]] = player
+            print(a)
+            print("You got an ATTACK up! \n")
+            attackUp()
+            yoinks()
+            return
+
         elif boards[tile][playerPos[0]][playerPos[1]-1] == monster:
+            health = healthMax[hpUpp]
             boards[tile][playerPos[0]][playerPos[1]] = floor
             playerPos[1] -= 1
             boards[tile][playerPos[0]][playerPos[1]] = player
@@ -283,7 +433,28 @@ def yoinks():
             yoinks()
             return
 
+        elif boards[tile][playerPos[0]][playerPos[1]+1] == hpUpItem:
+            boards[tile][playerPos[0]][playerPos[1]] = floor
+            playerPos[1] += 1
+            boards[tile][playerPos[0]][playerPos[1]] = player
+            print(a)
+            print("You got an HP up! \n")
+            hpUp()
+            yoinks()
+            return
+
+        elif boards[tile][playerPos[0]][playerPos[1]+1] == attUpItem:
+            boards[tile][playerPos[0]][playerPos[1]] = floor
+            playerPos[1] += 1
+            boards[tile][playerPos[0]][playerPos[1]] = player
+            print(a)
+            print("You got an ATTACK up! \n")
+            attackUp()
+            yoinks()
+            return
+
         elif boards[tile][playerPos[0]][playerPos[1]+1] == monster:
+            health = healthMax[hpUpp]
             boards[tile][playerPos[0]][playerPos[1]] = floor
             playerPos[1] += 1
             boards[tile][playerPos[0]][playerPos[1]] = player
@@ -326,4 +497,5 @@ def yoinks():
 
 
 print(a)
+powerUpf()
 yoinks()
