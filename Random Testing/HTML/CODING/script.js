f1f59dbf1fb0519807478ff248e87f238fc91526
Toaster <ttoasterrr@gmail.com>
2018-09-27 19:38:49 -0600

    Added CODING

---
+++ Random Testing/HTML/CODING/script.js
@@ -0,0 +1,19 @@
+function edit() {
+	window.open('','_self').close()
+	window.open('https://repl.it/@TToasterr/CODING')
+}
+
+function repl() {
+	window.open('','_self').close()
+	window.open('https://repl.it/repls')
+}
+
+function so() {
+	window.open('','_self').close()
+	window.open('http://stackoverflow.com/')
+}
+
+// function () {
+// 	window.open('','_self').close()
+// 	window.open('')
+// }
