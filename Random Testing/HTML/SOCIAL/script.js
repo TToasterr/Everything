d1c4eb55563f30b0e053df2f3abfbea2e8acdb32
Toaster <ttoasterrr@gmail.com>
2018-09-27 19:27:41 -0600

    Added script and fixed background colors

---
+++ Random Testing/HTML/SOCIAL/script.js
@@ -0,0 +1,19 @@
+function edit() {
+	window.open('','_self').close()
+	window.open('https://repl.it/@TToasterr/SOCIAL')
+}
+
+function youtube() {
+	window.open('','_self').close()
+	window.open('https://youtube.com')
+}
+
+function reddit() {
+	window.open('','_self').close()
+	window.open('https://reddit.com')
+}
+
+function monday() {
+	window.open('','_self').close()
+	window.open('https://ltjn.monday.com/boards/132160709/')
+}
