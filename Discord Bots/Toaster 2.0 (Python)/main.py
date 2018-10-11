97b69401dd68ebcceba3e5d96cd0ee3cd4dbe0ec
Toaster <ttoasterrr@gmail.com>
2018-10-11 06:55:49 -0600

    created toaster 2.0

---
+++ Discord Bots/Toaster 2.0 (Python)/main.py
@@ -0,0 +1,38 @@
+import discord
+import sys
+sys.path.append("H:/Misc")
+
+client = discord.Client()
+help = """**.help** - shows this"""
+
+
+
+def msg(msg):
+    return msg
+
+
+
+@client.event
+async def on_ready():
+    activeServers = client.servers
+    sum = 0
+    for s in activeServers:
+        sum += len(s.members)
+    print("Bot started in %s server(s), with %s users." % (len(client.servers), sum))
+    await client.change_presence(game=discord.Game(name="Toaster 2.0 | .help"))
+
+
+
+@client.event
+async def on_message(message):
+    msg = msg()
+
+    if msg == "-help":
+        await client.send_message(message.channel, content = help)
+        print("%s asked for help." % message.author)
+
+
+
+with open('H:/Misc/token3.txt', 'r') as myfile:
+    token = myfile.read()
+client.run(token)
