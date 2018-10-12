e8e4c9e8e84d6a69d1b7b7188492a5f6a7f448bb
Toaster <ttoasterrr@gmail.com>
2018-10-11 22:41:21 -0600

    DID A SHIT TON OF STUFF (note - bot doesnt like stalking on new servers)

--- Discord Bots/Toaster 2.0 (Python)/main.py
+++ Discord Bots/Toaster 2.0 (Python)/main.py
@@ -4,35 +4,75 @@ sys.path.append("H:/Misc")
 
 client = discord.Client()
 help = """**.help** - Shows this.
-**.invite** - Gives you an easy invite link for the bot."""
+**.invite** - Gives you an easy invite link for the bot.
+**.stalk** - Turns on stalking for this server.""" #The commands (what shows up when you do .help)
 
 
 
 @client.event
-async def on_ready():
+async def on_ready(): #when the bot starts up
+    activeServers = client.servers #activeServers is the servers the bot is in
+    sum = 0 #sum = 0
+    for s in activeServers: #for each server in active Servers
+        sum += len(s.members) #get the member count and add it to the sum.
+    print("Bot started in %s server(s), with %s users." % (len(client.servers), sum)) #print the server count and member count
+    await client.change_presence(game=discord.Game(name="Toaster 2.0 | .help")) #change presence to show the help command
+
+
+
+@client.event
+async def on_message(message): #when a message is sent
+    global content
+
     activeServers = client.servers
-    sum = 0
-    for s in activeServers:
-        sum += len(s.members)
-    print("Bot started in %s server(s), with %s users." % (len(client.servers), sum))
-    await client.change_presence(game=discord.Game(name="Toaster 2.0 | .help"))
+    msg = message.content #msg is the message the user sent
 
+    #Try to open the servers file (stores data about whether its stalking or not and such)
+    try:
+        with open(("%s.txt" % message.server.name), "r") as sFile:
+            if message.author.bot: #stop doin this gay shit if its a bot
+                return()
+            a = sFile.read().split(", ")
+            if a[1] == "1":
+                print("%s | #%s | %s: %s" % (message.server.name, message.channel, message.author, message.content))
+    #If it cant find the file, create one
+    except:
+        with open(("%s.txt" % message.server.name), "w+") as serverFile:
+            asdfaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa = "i want to die please kthx" #dont I just have the best code
 
-
-@client.event
-async def on_message(message):
-    msg = message.content
-
+    #If the message is the help command, send help
     if msg == ".help":
         await client.send_message(message.channel, content = help)
         print("%s asked for help." % message.author)
 
+    #Invite command (gives invite link)
     if msg == ".invite":
         await client.send_message(message.channel, content = "https://discordapp.com/oauth2/authorize?client_id=499928971711086601&scope=bot")
         print("%s got an invite link." % message.author)
 
+    #Stalk command (prints every message sent into a server into console if on)
+    if msg == ".stalk":
+        print("%s toggled stalking for %s." % (message.author, message.server.name))
+
+        with open(("%s.txt" % message.server.name), "r") as sFile:
+            content = sFile.read().split(", ")
+
+        with open(("%s.txt" % message.server.name), "w+") as serverFile:
+            if content == [""]:
+                serverFile.write("%s, 1" % message.server.name)
+            else:
+
+                if content[1] == "1":
+                    content[1] = "0"
+                    await client.send_message(message.channel, content = "Stalking turned off for **%s**." % message.server.name)
+                else:
+                    content[1] = "1"
+                    await client.send_message(message.channel, content = "Stalking turned on for **%s**." % message.server.name)
+                serverFile.write(", ".join(content))
 
 
+
+#my super spooky way of hiding my token in a local file xd
 with open('H:/Misc/token3.txt', 'r') as myfile:
     token = myfile.read()
 client.run(token)
