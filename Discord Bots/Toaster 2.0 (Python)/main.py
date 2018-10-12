c1055a6749ba126811e3ba95b87a931e3660cf7c
Toaster <ttoasterrr@gmail.com>
2018-10-12 16:58:15 -0600

    i want to die hydrogen can you fuck off

--- Discord Bots/Toaster 2.0 (Python)/main.py
+++ Discord Bots/Toaster 2.0 (Python)/main.py
@@ -5,8 +5,11 @@ sys.path.append("H:/Misc")
 client = discord.Client()
 help = """**.help** - Shows this.
 **.invite** - Gives you an easy invite link for the bot.
+**.addrole [id]** - Adds a moderator role by ID. To get a role id, ping it and add a backslash before the @ sign.
 **.stalk** - Turns on stalking for this server.
-**.vw [message], [spaces amount]** - Vaporwaves your message with the specified amount of spaces.""" #The commands (what shows up when you do .help)
+**.vw [message], [spaces amount]** - Vaporwaves your message with the specified amount of spaces.
+**.addresponse [trigger], [response]** - Adds an autoresponder response.
+**.delresponse [name] - Removes the responses with said name.""" #The commands (what shows up when you do .help)
 
 
 
@@ -27,10 +30,11 @@ async def on_message(message): #when a message is sent
 
     activeServers = client.servers
     msg = message.content #msg is the message the user sent
+    mod = False
 
     #Try to open the servers file (stores data about whether its stalking or not and such)
     try:
-        with open(("%s-stalking.txt" % message.server.name), "r+") as sFile:
+        with open(("%s-stalking.txt" % message.server.name), "r") as sFile:
             if message.author.bot: #stop doin this gay shit if its a bot
                 return()
             a = sFile.read()
@@ -39,21 +43,48 @@ async def on_message(message): #when a message is sent
     #If it cant find the file, create one
     except:
         with open(("%s-stalking.txt" % message.server.name), "w+") as stalkingFile:
-            stalkingFile.write("0" % message.server.name) #dont I just have the best code
+            stalkingFile.write("0" % message.server.name)
 
-    #If the message is the help command, send help
+
+
+    try:
+        with open(("%s-modRoles.txt" % message.server.name), "r") as modFile:
+            modRoles = modFile.read().split("\n")
+
+    except:
+        with open(("%s-modRoles.txt" % message.server.name), "w+") as stalkingFile:
+            stalkingFile.write("")
+
+
+
+    #Help command
     if msg == ".help":
         await client.send_message(message.channel, content = help)
-        print("%s asked for help." % message.author)
+        print("%s asked for help.\n" % message.author)
+
+
 
     #Invite command (gives invite link)
     if msg == ".invite":
-        await client.send_message(message.channel, content = "https://discordapp.com/oauth2/authorize?client_id=499928971711086601&scope=bot")
-        print("%s got an invite link." % message.author)
+        await client.send_message(message.channel, content = "https://discordapp.com/oauth2/authorize?client_id=[SPOOKY ID HERE]&scope=bot")
+        print("%s got an invite link.\n" % message.author)
+
+
 
     #Stalk command (prints every message sent into a server into console if on)
     if msg == ".stalk":
-        print("%s toggled stalking for %s." % (message.author, message.server.name))
+        for i in modRoles:
+            if i in [y.id for y in message.author.roles]:
+                mod = True
+
+        if message.author == "Toaster#2600":
+            mod = True
+
+        if not mod:
+            await client.send_message(message.channel, content = "You dont have the right role to do this.")
+            return()
+
+        print("%s toggled stalking for %s.\n" % (message.author, message.server.name))
 
         with open(("%s-stalking.txt" % message.server.name), "r") as sFile:
             content = sFile.read()
@@ -71,6 +102,8 @@ async def on_message(message): #when a message is sent
                     await client.send_message(message.channel, content = "Stalking turned on for **%s**." % message.server.name)
                 stalkingFile.write(content)
 
+
+
     #vaporwave command
     if msg[:3] == ".vw":
         if not message.channel.is_private:
@@ -79,7 +112,7 @@ async def on_message(message): #when a message is sent
         mesg = args[0]
         spaceamount = args[1]
         final = []
-        print("%s vaporwaved \"%s\" with %s space(s)." % (message.author, mesg, spaceamount))
+        print("%s vaporwaved \"%s\" with %s space(s).\n" % (message.author, mesg, spaceamount))
 
         for char in mesg:
             final.append(char)
@@ -87,6 +120,90 @@ async def on_message(message): #when a message is sent
         spaces = " " * int(spaceamount)
         final = spaces.join(final)
         await client.send_message(message.channel, content = final)
+
+
+
+    #server specific autoresponder
+    if msg[:12] == ".addresponse":
+        for i in modRoles:
+            if i in [y.id for y in message.author.roles]:
+                mod = True
+
+        if message.author == "Toaster#2600":
+            mod = True
+
+        if not mod:
+            await client.send_message(message.channel, content = "You dont have the right role to do this.")
+            return()
+
+        msg = msg[13:].split(", ")
+        trigger = msg[0]
+        response = msg[1]
+
+        with open(("%s-autoresponder.txt" % message.server.name), "a+") as arFile:
+            arFile.write("\n%s -> %s\n---" % (trigger, response))
+
+        await client.send_message(message.channel, content = "Your autoresponder has been added!")
+        print("%s just added an autoresponder to %s.\nTrigger: '%s'\nResponse: '%s'\n" % (message.author, message.server.name, trigger, response))
+
+
+
+    #more autoresponder
+    if msg[:12] == ".delresponse":
+        for i in modRoles:
+            if i in [y.id for y in message.author.roles]:
+                mod = True
+
+        if message.author == "Toaster#2600":
+            mod = True
+
+        if not mod:
+            await client.send_message(message.channel, content = "You dont have the right role to do this.")
+            return()
+
+        trigger = msg[13:]
+        removed = 0
+        popnum = []
+        numb = 0
+
+        with open(("%s-autoresponder.txt" % message.server.name), "r") as arFile:
+            ar = arFile.read().split("---")
+            print(ar)
+
+        for num in range(len(ar) - 1):
+            print(num)
+            i = ar[num].split(" -> ")
+            if str(i[0])[1:] == trigger:
+                popnum[numb] = num
+                numb += 1
+                removed += 1
+
+        for num in popnum:
+            ar.pop(num)
+
+        with open(("%s-autoresponder.txt" % message.server.name), "w+") as arFile:
+            arFile.write("---".join(ar))
+
+        if removed > 0:
+            await client.send_message(message.channel, content = "%s triggers have been removed." % removed)
+            print("%s removed %s triggers from %s.\n" % (message.author, removed, message.server.name))
+        else:
+            await client.send_message(message.channel, content = "There werent any triggers with that name.")
+
+
+
+    #Add a moderator role
+    if msg[:8] == ".addrole":
+        role = msg[9:]
+
+        with open(("%s-modRoles.txt" % message.server.name), "a+") as modFile:
+            modFile.write("%s\n" % role)
+
+        await client.send_message(message.channel, content = "Successfully added @%s to the mod roles." % role)
+        print("%s just added %s to the mod roles for %s.\n" % (message.author, role, message.server.name))
+
+
+
 
 
 
