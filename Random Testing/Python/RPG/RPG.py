d9ec967c306e8a505e56c9d60e3811ed3e1645a5
Toaster <38060892+TToasterr@users.noreply.github.com>
2018-10-25 08:54:00 -0600

    inspecting and giving items, as well as stats

--- Random Testing/Python/RPG/RPG.py
+++ Random Testing/Python/RPG/RPG.py
@@ -3,8 +3,6 @@ from random import choice as ch
 import allitems
 
 
-weapons = ["Melee","Ranged","Magic"]
-
 
 def newline():
     print("")
@@ -25,12 +23,35 @@ class Player:
         self.x = x
         self.y = y
 
+    def printEquipped(self):
+        try:
+            print("Main: %s" % self.equipped["main"].name)
+        except:
+            print("Main: ")
+        try:
+            print("Side: %s" % self.equipped["side"].name)
+        except:
+            print("Side: ")
+        try:
+            print("Armor: %s" % self.equipped["armor"].name)
+        except:
+            print("Armor: ")
+
     def printBasics(self):
         print("Name: %s" % self.name)
         newline()
-        print("Main: %s" % self.equipped["main"])
-        print("Side: %s" % self.equipped["side"])
-        print("Armor: %s" % self.equipped["armor"])
+        try:
+            print("Main: %s" % self.equipped["main"].name)
+        except:
+            print("Main: ")
+        try:
+            print("Side: %s" % self.equipped["side"].name)
+        except:
+            print("Side: ")
+        try:
+            print("Armor: %s" % self.equipped["armor"].name)
+        except:
+            print("Armor: ")
 
         if self.questing:
             newline()
@@ -42,7 +63,7 @@ class Player:
     def printInv(self):
         print("Inventory: ")
         for item in self.inv:
-            item.printItem()
+            item.printName()
         newline()
 
 
@@ -53,10 +74,12 @@ equipped = {
 }
 inv = [allitems.minor_heal_pot]
 quest = {
-    "goal":"be gay",
-    "item":"the gay totem"
+    "goal":"",
+    "item":""
 }
-player = Player(input("What would you like your players name to be?\n"), equipped, inv, True, quest, 0, 0)
+bigboi()
+name = input("What would you like your players name to be?\n")
+player = Player(name, equipped, inv, False, quest, 0, 0)
 
 
 bigboi()
@@ -66,26 +89,87 @@ while True:
     selection = input ("what would you like to do? \n('help' for help) \n")
     #call funcs with input
     bigboi()
-    if selection == "move":
+
+    if selection == "stats":
+        player.printBasics()
+        newline()
+
+    elif selection == "move":
         print("pretend you moved")
         newline()
+
     elif selection == "toggle minimap":
         print("pretend you toggled your minimap")
         newline()
+
     elif selection == "look around":
         print("wow the void looks nice today")
         newline()
-    elif selection == "inventory":
+
+    elif selection == "inspect item":
+        found = 0
+        player.printEquipped()
+        newline()
         player.printInv()
         newline()
+        which = input("Which item would you like to inspect?\n")
+        newline()
+        for item in player.inv:
+            if found == 1:
+                do = "nothing"
+            elif item.name == which:
+                bigboi()
+                found = 1
+                item.printItem()
+                newline()
+        for item in player.equipped:
+            if found == 1:
+                do = "nothing"
+            else:
+                try:
+                    if player.equipped[item].name == which:
+                        bigboi()
+                        found = 1
+                        player.equipped[item].printItem()
+                        newline()
+                except:
+                    do = "nothing"
+        if found == 0:
+            bigboi()
+            print("There isnt an item with that name!")
+            newline()
+
+    elif selection == "inventory" or selection == "inv":
+        player.printInv()
+        newline()
+
     elif selection == "help":
         print("List of Commands:")
+        print("stats")
         print("move")
         print("toggle minimap")
         print("look around")
+        print("inspect item")
         print("inventory")
         print("help")
         newline()
+
+    elif selection == "giveItem":
+        name = input("Whats the item name?\n")
+        newline()
+        power = input("Whats the item power?\n")
+        newline()
+        type = input("Whats the item type?\n")
+        newline()
+        rarity = input("Whats the item rarity?\n")
+        newline()
+        bufftype = input("Whats the item bufftype?\n")
+        newline()
+        buffammount = input("Whats the buff amount?\n")
+        bigboi()
+        item = allitems.Item(name, power, type, rarity, bufftype, buffammount)
+        player.inv.append(item)
     else:
+
         print("That's not a command!")
         newline()
