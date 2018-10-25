fa29c27588f4805067aa7be5f739d1a137d50cc2
Toaster <38060892+TToasterr@users.noreply.github.com>
2018-10-25 09:19:17 -0600

    Added healing potion types

--- Random Testing/Python/RPG/allitems.py
+++ Random Testing/Python/RPG/allitems.py
@@ -29,7 +29,30 @@ class Item:
             print("%s")
 
 
-minor_heal_pot = Item("Minor Healing Potion", 5, "Potion", "Very Common", "None", 0)
+minor_heal_pot = Item("Minor Healing Potion", 10, "Potion", "Very Common", "None", 0)
+lesser_heal_pot = Item("Lesser Healing Potion", 20, "Potion", "Common", "None", 0)
+heal_pot = Item("Healing Potion", 50, "Potion", "Uncommon", "None", 0)
+greater_heal_pot = Item("Greater Healing Potion", 100, "Potion", "Rare", "None", 0)
+massive_heal_pot = Item("Massive Healing Potion", 200, "Potion", "Legendary", "None", 0)
 
 rusty_sword = Item("Rusty Sword", 2, "Melee", "Starter", "None", 0)
 rusty_chestplate = Item("Rusty Chestplate", 3, "Armor", "Starter", "None", 0)
+
+item_array = [
+    minor_heal_pot,
+    lesser_heal_pot,
+    heal_pot,
+    greater_heal_pot,
+    massive_heal_pot,
+    rusty_sword,
+    rusty_chestplate
+]
+item_name_array = [
+    "minor_heal_pot",
+    "lesser_heal_pot",
+    "heal_pot",
+    "greater_heal_pot",
+    "massive_heal_pot",
+    "rusty_sword",
+    "rusty_chestplate"
+]
