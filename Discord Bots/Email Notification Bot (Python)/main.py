eb4ef79d668dc908243f34770226e093a0e972fb
Toaster <ttoasterrr@gmail.com>
2018-11-22 20:22:58 -0700

    Fixed stuff, but it only worked for one person.

--- Discord Bots/Email Notification Bot (Python)/main.py
+++ Discord Bots/Email Notification Bot (Python)/main.py
@@ -2,14 +2,16 @@ from datetime import datetime
 from random import randint as ri
 from random import choice as ch
 import discord
-import sys
 import easyimap
 import smtplib
 import subprocess
+import sys
 sys.path.append("H:/Misc")
 
 client = discord.Client()
 placeholder = []
+last_5_emails = []
+current_5_emails = []
 
 with open ("H:/Misc/emaillogins.txt", "r+") as logins:
     all_logins = logins.read().split("\n")
@@ -26,15 +28,16 @@ for user_login in all_logins:
     else:
         login = user_login[0]
         password = user_login[1]
+        user_id = user_login[2]
 
         try:
             imapper = easyimap.connect('imap.gmail.com', login, password)
         except:
             print("Couldnt log into %s.\n" % login)
 
-        for mail_id in imapper.listids(limit=1):
+        for mail_id in imapper.listids(limit=5):
             mail = imapper.mail(mail_id)
-            last_email_id = mail.message_id
+            last_5_emails.append(mail)
 # except:
 #     print("The email list was empty!")
 #     last_email_id = ""
@@ -60,8 +63,8 @@ async def on_server_join(server):
 
 @client.event
 async def on_message(message):
-    global last_email_id
-    email_id = ""
+    global last_5_emails
+    current_5_emails = []
 
     if message.author.bot:
         return()
@@ -72,7 +75,7 @@ async def on_message(message):
         passw = args[1]
 
         with open("H:/Misc/emaillogins.txt", "a+") as logins:
-            logins.write("%s|||%s\n" % (email, passw))
+            logins.write("%s|||%s|||%s\n" % (email, passw, message.author.id))
 
         print("%s added a new email to the bot." % message.author)
         print("Email: %s" % email)
@@ -91,37 +94,40 @@ async def on_message(message):
         all_logins = placeholder
 
     for user_login in all_logins:
-        if user_login == [""]:
-            do = "nothing"
-        else:
+        if user_login != [""]:
             login = user_login[0]
             password = user_login[1]
+            user_id = user_login[2]
 
-            smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
-            smtpObj.ehlo()
-            smtpObj.starttls()
-            # try:
-            smtpObj.login(login, password)
-            # except:
-            #     await client.send_message(message.channel, content = "You haven't enabled less secure apps for the account **%s**!\nPlease do so to use the bot." % (login))
-            #     return()
+            current_5_emails = []
 
-            imapper = easyimap.connect('imap.gmail.com', login, password)
+            if message.author.id == user_id:
+                smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+                smtpObj.ehlo()
+                smtpObj.starttls()
+                # try:
+                smtpObj.login(login, password)
+                # except:
+                #     await client.send_message(message.channel, content = "You haven't enabled less secure apps for the account **%s**!\nPlease do so to use the bot." % (login))
+                #     return()
 
-            for mailid in imapper.listids(limit=1):
-                mail = imapper.mail(mailid)
-                email_id = mail.message_id
+                imapper = easyimap.connect('imap.gmail.com', login, password)
 
-            if email_id != last_email_id:
-                if last_email_id == "":
+                for mailid in imapper.listids(limit=5):
+                    mail = imapper.mail(mailid)
+                    current_5_emails.append(mail)
+
+                if last_5_emails == [] or last_5_emails == [""] or last_5_emails == "":
                     await client.send_message(message.channel, content = "Logged into **%s** succesfully!" % login)
                     return
-                last_email_id = email_id
-                for mail_id in imapper.listids(limit=1):
-                    mail = imapper.mail(mail_id)
-                await client.send_message(message.channel, content = "<@%s>\n**From:** %s\n**Subject:** %s\n**Body:** %s" % (message.author.id, mail.from_addr, mail.title, mail.body))
 
-                print("%s got a new email!\n" % message.author)
+                print([email.title for email in current_5_emails])
+                for email_num in range(len(current_5_emails)):
+                    print(email_num)
+                    if current_5_emails[email_num].message_id != last_5_emails[email_num].message_id:
+                        await client.send_message(message.channel, content = "<@%s>\n**From:** %s\n**Subject:** %s\n**Body:** %s" % (message.author.id, current_5_emails[email_num].from_addr, current_5_emails[email_num].title, current_5_emails[email_num].body))
+                        print("%s got a new email!\n" % message.author)
+                last_5_emails = current_5_emails
 
 
 
