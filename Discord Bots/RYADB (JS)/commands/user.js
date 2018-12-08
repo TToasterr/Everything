05eb91b048972159c46b4954150928c615feea81
Toaster <ttoasterrr@gmail.com>
2018-12-07 19:39:12 -0700

    Fixed name

--- Discord Bots/RYADB (JS)/commands/user.js
+++ Discord Bots/RYADB (JS)/commands/user.js
@@ -11,7 +11,7 @@ module.exports = {
       const tagged = message.author;
       const final = new Discord.RichEmbed()
       .setColor('#00ff00')
-      .setAuthor('YABOT (Yet Another Bot Of Toasters)', 'https://cdn.discordapp.com/avatars/184474965859368960/5325a0eed911e9f09e24fd277e886846.png?size=2048', 'https://discordapp.com/api/oauth2/authorize?client_id=519640938667048960&permissions=8&scope=bot')
+      .setAuthor('YABOT (Yet Another Bot Of Toaster\'s)', 'https://cdn.discordapp.com/avatars/184474965859368960/5325a0eed911e9f09e24fd277e886846.png?size=2048', 'https://discordapp.com/api/oauth2/authorize?client_id=519640938667048960&permissions=8&scope=bot')
       .addField('STATS', `**Username** - ${tagged.username}\n**Created** - ` + `${tagged.createdAt}`.substring(0,16) + `\n**ID** - ${tagged.id}\n\n`)
       .addBlankField()
       .addField('AVATAR', 'v')
@@ -25,7 +25,7 @@ module.exports = {
       const tagged = message.mentions.users.first();
       const final = new Discord.RichEmbed()
       .setColor('#00ff00')
-      .setAuthor('YABOT (Yet Another Bot Of Toasters)', 'https://cdn.discordapp.com/avatars/184474965859368960/5325a0eed911e9f09e24fd277e886846.png?size=2048', 'https://discordapp.com/api/oauth2/authorize?client_id=519640938667048960&permissions=8&scope=bot')
+      .setAuthor('YABOT (Yet Another Bot Of Toaster\'s)', 'https://cdn.discordapp.com/avatars/184474965859368960/5325a0eed911e9f09e24fd277e886846.png?size=2048', 'https://discordapp.com/api/oauth2/authorize?client_id=519640938667048960&permissions=8&scope=bot')
       .addField('STATS', `**Username** - ${tagged.username}\n**Created** - ` + `${tagged.createdAt}`.substring(0,16) + `\n**ID** - ${tagged.id}\n\n`)
       .addBlankField()
       .addField('AVATAR', 'v')
