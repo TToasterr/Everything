6326782970fbeef0a5b8231e94f38f4094ced427
Toaster <ttoasterrr@gmail.com>
2018-12-07 19:39:02 -0700

    Fixed name

--- Discord Bots/RYADB (JS)/commands/invite.js
+++ Discord Bots/RYADB (JS)/commands/invite.js
@@ -9,7 +9,7 @@ module.exports = {
   execute(message, args) {
     const final = new Discord.RichEmbed()
     .setColor('#00ff00')
-    .setAuthor('YABOT (Yet Another Bot Of Toasters)', 'https://cdn.discordapp.com/avatars/184474965859368960/5325a0eed911e9f09e24fd277e886846.png?size=2048', 'https://discordapp.com/api/oauth2/authorize?client_id=519640938667048960&permissions=8&scope=bot')
+    .setAuthor('YABOT (Yet Another Bot Of Toaster\'s)', 'https://cdn.discordapp.com/avatars/184474965859368960/5325a0eed911e9f09e24fd277e886846.png?size=2048', 'https://discordapp.com/api/oauth2/authorize?client_id=519640938667048960&permissions=8&scope=bot')
     .setTitle('Invite Link')
     .setDescription('Add me to your server by clicking my name above!')
     message.channel.send(final);
