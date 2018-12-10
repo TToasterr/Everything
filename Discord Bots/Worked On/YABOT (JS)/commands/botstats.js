a252ee61b3929770c365ae1a9fd8122cb3307323
Toaster <ttoasterrr@gmail.com>
2018-12-10 05:34:08 -0700

    Made botstats.js

---
+++ Discord Bots/Worked On/YABOT (JS)/commands/botstats.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js');
+
+module.exports = {
+  name: 'botstats',
+  description: 'Get the stats for the bot!',
+  usage: '',
+  category: 'general',
+  guildOnly: false,
+  args: false,
+  mod: false,
+  execute(message, args) {
+    // const final = new Discord.RichEmbed()
+    // .setColor('#000000')
+    // .setAuthor('YABOT (Yet Another Bot Of Toaster\'s)', 'https://cdn.discordapp.com/avatars/184474965859368960/5325a0eed911e9f09e24fd277e886846.png?size=2048', 'https://discordapp.com/api/oauth2/authorize?client_id=519640938667048960&permissions=8&scope=bot')
+    // .setTitle('')
+    // .setDescription('')
+    // .addField('', '');
+    // message.channel.send(final);
+    // console.log(`${message.author.username} did something.`);
+  },
+};
