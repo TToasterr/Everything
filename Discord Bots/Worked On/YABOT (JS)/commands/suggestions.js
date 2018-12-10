2ab12c1a1d237e8ea18a9a3fd0695ca29aad31cb
Toaster <ttoasterrr@gmail.com>
2018-12-10 15:08:42 -0700

    Formatted suggestion listing

--- Discord Bots/Worked On/YABOT (JS)/commands/suggestions.js
+++ Discord Bots/Worked On/YABOT (JS)/commands/suggestions.js
@@ -39,7 +39,7 @@ module.exports = {
           });
         }
         var suggestionn = JSON.parse(fileContent);
-        finalEmbed.addField(i + ' - ' + suggestionn.status, `**By:** ${suggestionn.who}**\n--------------`);
+        finalEmbed.addField(i + ' - ' + suggestionn.who, `**Status:** ${suggestionn.status}\n--------------`);
       }
     }
     message.channel.send(finalEmbed);
