d1adfec87e60af94044d811f635259fdfc14d91a
Toaster <ttoasterrr@gmail.com>
2019-03-10 20:52:36 -0600

    y  e  e  t

--- Discord Bots/Worked On/Marv/main.js
+++ Discord Bots/Worked On/Marv/main.js
@@ -34,19 +34,11 @@ client.on(`message`, message => {
 				i = `0${i}`;
 			}
 
-			// var splitMessage = message.content.split(i);
-			// var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-			// console.log(`${i}	-	${splitMessage}`)
-
 			let final = new Discord.RichEmbed()
 			.setColor(`#000000`)
 			.setAuthor(`Marv`, `https://images.discordapp.net/avatars/538173713162567690/07187402dab82f0fd34348a0a5202ecc.png?size=512`, `https://discordapp.com/api/oauth2/authorize?client_id=554490018752626708&permissions=8&scope=bot`)
 
-			// if (message.content.includes(`&{i}`) && !(numbers.includes(splitMessage[0][-1])) && !(numbers.includes(splitMessage[1][0]))) {
 			if (message.content.includes(`SCP-${i}`)) {
-				// console.log(splitMessage[0][-1])
-				// console.log(splitMessage[1][0])
 				final.setDescription(`[\`SCP-${i}\`](http://www.scp-wiki.net/scp-${i})`);
 				message.channel.send(final);
 				done = true;
