876515d4408226512b0ad2144917892f7658d93f
Toaster <ttoasterrr@gmail.com>
2019-03-13 11:31:09 -0600

    b

--- Discord Bots/Worked On/Silicon/main.js
+++ Discord Bots/Worked On/Silicon/main.js
@@ -7,4 +7,11 @@ const client = new Discord.Client();
 client.once(`ready`, () => {
         console.log(`\n\n\n\n\n\n\n\n\n\n\n\n\n`);
         console.log(`SILICON IS RUNNING\nVISIBLE USERS: ${client.users.size}\nVISIBLE CHANNELS: ${client.channels.size}\nVISIBLE SERVERS: ${client.guilds.size}\n`);
+        client.user.setActivity(`slc.help`);
+});
+
+// -----------------------------------------------------------------------------
+
+client.on(`guildCreate`, guild => {
+        console.log(`\nNEW SERVER VISIBLE: ${guild.name}\nNEW USERS VISIBLE:  ${guild.memberCoutn}\n`);
 });
