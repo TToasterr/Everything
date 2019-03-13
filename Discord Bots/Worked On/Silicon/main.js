81611085d70cb2d5e501ef22ca98f18411b99e31
Toaster <38060892+TToasterr@users.noreply.github.com>
2019-03-13 10:25:40 -0600

    Silicon Base created

---
+++ Discord Bots/Worked On/Silicon/main.js
@@ -0,0 +1,9 @@
+const fs = require(`fs`);
+const Discord = require(`discord.js`);
+const client = new Discord.Client();
+
+// -----------------------------------------------------------------------------
+
+client.once(`ready`, () => {
+	console.log(`\n\n\n\n\n\n\n\n\n\n`);
+});
