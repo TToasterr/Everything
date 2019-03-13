0ae4102c3a6319ba1865f1c2474b812b62740b79
Toaster <ttoasterrr@gmail.com>
2019-03-13 10:30:40 -0600

    a

--- Discord Bots/Worked On/Silicon/main.js
+++ Discord Bots/Worked On/Silicon/main.js
@@ -5,5 +5,6 @@ const client = new Discord.Client();
 // -----------------------------------------------------------------------------
 
 client.once(`ready`, () => {
-	console.log(`\n\n\n\n\n\n\n\n\n\n`);
+        console.log(`\n\n\n\n\n\n\n\n\n\n\n\n\n`);
+        console.log(`SILICON IS RUNNING\nVISIBLE USERS: ${client.users.size}\nVISIBLE CHANNELS: ${client.channels.size}\nVISIBLE SERVERS: ${client.guilds.size}\n`);
 });
