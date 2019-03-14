72dad0362cff04426457a8bae8754918daf360a0
GoldenPot8o <42702376+GoldenPot8o@users.noreply.github.com>
2019-03-14 11:14:04 -0600

    connection error xd
    aaaaa      a        aaaaaa  a a

--- Discord Bots/Worked On/Xenon/nate/voicechannel.js
+++ Discord Bots/Worked On/Xenon/nate/voicechannel.js
@@ -1,17 +1,25 @@
 const Discord = require('discord.js');
 
 module.exports = {
-        name: '',
-        description: '',
-        usage: '',
-        guildOnly: false,
+        name: 'play',
+        description: 'it does a thing with sound',
+        usage: '<video link>',
+        guildOnly: true,
         args: false,
         mod: false,
         execute(message, args, client, time, final) {
+		if (message.member.voice.channel) {
+			const connection = await message.member.voice.channel.join();
+		} else {
+			final.setTitle('__**wow ur so bad xd**__');
+				.setDescription('wow what a nerd you arent even in a noise place');
+			return console.log(`[${time}] ${message.author.username} tried to add the bot to a voice channel.`);
+		}
+                
                 // final.setTitle(``)
                 // .setDescription(``)
                 // .addField(``, ``);
-                // message.channel.send(final);
-                // console.log(`[${time}] ${message.author.username} did something.`);
+                message.channel.send(final);
+                console.log(`[${time}] ${message.author.username} added the bot to a voice channel.`);
         },
 };
