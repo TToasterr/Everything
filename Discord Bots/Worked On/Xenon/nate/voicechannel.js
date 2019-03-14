35ca02592c62444a87ad4675d4b2cb0d6d1ee92c
Toaster <38060892+TToasterr@users.noreply.github.com>
2019-03-14 11:25:10 -0600

    finished xd

--- Discord Bots/Worked On/Xenon/nate/voicechannel.js
+++ Discord Bots/Worked On/Xenon/nate/voicechannel.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const ytdl = require('ytdl-core');
 
 module.exports = {
         name: 'play',
@@ -8,16 +9,19 @@ module.exports = {
         args: false,
         mod: false,
         execute(message, args, client, time, final) {
+		var connection;
 		if (message.member.voice.channel) {
-			const connection = await message.member.voice.channel.join();
+			connection = await message.member.voice.channel.join();
 		} else {
 			final.setTitle('__**wow ur so bad xd**__');
 				.setDescription('wow what a nerd you arent even in a noise place');
 			return console.log(`[${time}] ${message.author.username} tried to add the bot to a voice channel.`);
 		}
+		
+		connection.play(ytdl(args[0].substring(1), { filter: 'audioonly' }));
                 
-                // final.setTitle(``)
-                // .setDescription(``)
+                final.setTitle(`__**Joined voice channel!**__`)
+                	.setDescription(`Now playing song ${args[0].substring(1)}`);
                 // .addField(``, ``);
                 message.channel.send(final);
                 console.log(`[${time}] ${message.author.username} added the bot to a voice channel.`);
