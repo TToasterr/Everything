3efda76f72a71a8545ba4a7f5d44ca1fb2b425cc
Toaster <ttoasterrr@gmail.com>
2019-04-10 06:47:01 -0600

    100 Hour Day Works!

--- Folder Projects/Worked On/100 Hour Day/main.js
+++ Folder Projects/Worked On/100 Hour Day/main.js
@@ -2,16 +2,17 @@ let hour = 0;
 let minute = 0;
 let second = 0;
 
-let date = new Date();
-let time = date.toString().substring(16, 24);
-
 
 // for (var i = 0; i < 1440; i++) {
 // 	if (14.24)
 // }
+// process.stdout.write('\033c');
 
 
 var mainLoop = setInterval(function() {
+	let date = new Date();
+	let time = date.toString().substring(16, 24);
+
 	second++;
 
 	if (second >= 100) {
@@ -28,5 +29,23 @@ var mainLoop = setInterval(function() {
 		hour = 0;
 	}
 
-	console.log(`\n\n${hour}:${minute}:${second}`);
+	secondOut = `${second}`;
+	minuteOut = `${minute}`;
+	hourOut = `${hour}`;
+
+	if (second < 10) {
+		secondOut = `0${second}`;
+	}
+	if (minute < 10) {
+		minuteOut = `0${minute}`;
+	}
+	if (hour < 10) {
+		hourOut = `0${hour}`
+	}
+
+	process.stdout.write(`\x1b[0m\x1b[2J\n\n\
+\x1b[32m\
+Custom Time:      \x1b[0m${hourOut}:${minuteOut}:${secondOut}\n\
+\x1b[31m\
+Real Time:        \x1b[0m${time}\n\n`);
 }, 86.4);
