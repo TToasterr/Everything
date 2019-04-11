5a55dda3fcc8799d7661e55a249cf8bf2a99eea1
Toaster <ttoasterrr@gmail.com>
2019-04-11 06:51:27 -0600

    i want to die thanks

--- Folder Projects/Worked On/School Progress Bar/main2.js
+++ Folder Projects/Worked On/School Progress Bar/main2.js
@@ -15,7 +15,7 @@ mmnt.relativeTimeThreshold("ss", 0);
 
 let timeFormat = "HH:mm:ss";
 let sleep_time = mmnt("22:00:00", timeFormat);
-let wake_time = mmnt("6:45:00", timeFormat);
+let wake_time = mmnt("6:50:00", timeFormat);
 let school_start = mmnt("7:50:00", timeFormat);
 let school_end = mmnt("14:35:00", timeFormat);
 
@@ -28,33 +28,44 @@ while (true) {
 	let until_sleep = mmnt(sleep_time).fromNow();
 	let after_wake = mmnt(wake_time).toNow().replace(/in (.+) seconds/g, "$1 seconds ago");
 
-	let until_sleep_clean;
-	let after_wake_clean;
+	let until_sleep_clean, after_wake_clean, statement, day_length, sleep_length, percent_left, percent_through;
 
-	if (until_sleep.endsWith("seconds")) {
-		until_sleep_clean = parseInt(until_sleep.split(" ")[1]);
-		after_wake_clean = parseInt(after_wake.split(" ")[0]);
+	// -----------------------------------------------------------------------------
+
+	if (!until_sleep.endsWith("seconds")) {
+		until_sleep_clean = parseInt(until_sleep.split(" ")[0]);
+		after_wake_clean = 0;
+
+		let day_length = (until_sleep_clean + after_wake_clean);
+		let sleep_length = secs_in_day - day_length;
+
+		let percent_left = Math.round((until_sleep_clean / day_length) * 1000) / 10;
+		let percent_through = Math.round((after_wake_clean / day_length) * 1000) / 10;
+
+		statement = `You have ${percent_left}% left in the day, and\nYou are ${percent_through}% through the day.`;
 	}
 	else {
-		until_sleep_clean = parseInt(until_sleep.split(" ")[0]);
+		until_sleep_clean = parseInt(until_sleep.split(" ")[1]);
 		after_wake_clean = parseInt(after_wake.split(" ")[0]);
+
+		let day_length = (until_sleep_clean + after_wake_clean);
+		let sleep_length = secs_in_day - day_length;
+
+		let percent_left = Math.round((until_sleep_clean / day_length) * 1000) / 10;
+		let percent_through = Math.round((after_wake_clean / day_length) * 1000) / 10;
+
+		statement = `You have ${percent_through}% left in the night, and\nYou are ${percent_left}% through the night.`;
 	}
 
-	// console.log(`${until_sleep}\n${after_wake}\n\n${until_sleep_clean}\n${after_wake_clean}`);
+	// -----------------------------------------------------------------------------
 
-	let day_length = (until_sleep_clean + after_wake_clean);
-	let sleep_length = secs_in_day - day_length;
-
-	let percent_left = Math.round((until_sleep_clean / day_length) * 1000) / 10;
-	let percent_through = Math.round((after_wake_clean / day_length) * 1000) / 10;
-
-	console.log(`${day_length}\n${sleep_length}`)
+	// console.log(`${day_length}\n${sleep_length}`);
+	console.log(until_sleep);
 
 	// -----------------------------------------------------------------------------
 
 
-	process.stdout.write(`\n\nYou have ${percent_left}% left in the day, and
-you are ${percent_through}% through the day.\n`);
+	process.stdout.write(`\n\n${statement}\n`);
 	for (var i = 0; i < 100; i++) {
 		if (i < Math.floor(percent_through)) {
 			process.stdout.write('█');
@@ -65,11 +76,7 @@ you are ${percent_through}% through the day.\n`);
 	}
 
 
-	process.stdout.write(`\n\n
-Schleemp Time is ${until_sleep}
-
-Waek Time was ${after_wake}\n\n`);
-
+	process.stdout.write(`\n\n\nSchleemp Time is ${until_sleep}\nWaek Time was ${after_wake}\n\n`);
 	console.log("\n\n\n");
 
 	// -----------------------------------------------------------------------------
