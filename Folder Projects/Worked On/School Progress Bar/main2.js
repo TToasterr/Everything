7a76440ed6ff0480d490c4113f298e959ebf15b0
Toaster <ttoasterrr@gmail.com>
2019-04-10 22:02:25 -0600

    mostly working except after sleep time

--- Folder Projects/Worked On/School Progress Bar/main2.js
+++ Folder Projects/Worked On/School Progress Bar/main2.js
@@ -1,6 +1,6 @@
 // school starts at 7:50
 // school ends at 14:35
-// sleep starts at 10:30
+// sleep starts at 10:00
 // sleep ends at 6:45
 // 86400 seconds in a day
 
@@ -16,26 +16,44 @@ mmnt.relativeTimeThreshold("ss", 0);
 let timeFormat = "HH:mm:ss";
 let sleep_time = mmnt("22:00:00", timeFormat);
 let wake_time = mmnt("6:45:00", timeFormat);
+let school_start = mmnt("7:50:00", timeFormat);
+let school_end = mmnt("14:35:00", timeFormat);
 
 // -----------------------------------------------------------------------------
 
 while (true) {
+
+	process.stdout.write("\x1b[2J");
+
 	let until_sleep = mmnt(sleep_time).fromNow();
 	let after_wake = mmnt(wake_time).toNow().replace(/in (.+) seconds/g, "$1 seconds ago");
 
-	let until_sleep_clean = parseInt(until_sleep.split(" ")[1]);
-	let after_wake_clean = parseInt(after_wake.split(" ")[0]);
+	let until_sleep_clean;
+	let after_wake_clean;
 
-	let day_length = ((parseInt(until_sleep.split(" ")[1]) + parseInt(after_wake.split(" ")[0])));
+	if (until_sleep.endsWith("seconds")) {
+		until_sleep_clean = parseInt(until_sleep.split(" ")[1]);
+		after_wake_clean = parseInt(after_wake.split(" ")[0]);
+	}
+	else {
+		until_sleep_clean = parseInt(until_sleep.split(" ")[0]);
+		after_wake_clean = parseInt(after_wake.split(" ")[0]);
+	}
+
+	// console.log(`${until_sleep}\n${after_wake}\n\n${until_sleep_clean}\n${after_wake_clean}`);
+
+	let day_length = (until_sleep_clean + after_wake_clean);
 	let sleep_length = secs_in_day - day_length;
 
 	let percent_left = Math.round((until_sleep_clean / day_length) * 1000) / 10;
 	let percent_through = Math.round((after_wake_clean / day_length) * 1000) / 10;
 
+	console.log(`${day_length}\n${sleep_length}`)
+
 	// -----------------------------------------------------------------------------
 
 
-	process.stdout.write(`\x1b[2J\n\nYou have ${percent_left}% left in the day, and
+	process.stdout.write(`\n\nYou have ${percent_left}% left in the day, and
 you are ${percent_through}% through the day.\n`);
 	for (var i = 0; i < 100; i++) {
 		if (i < Math.floor(percent_through)) {
