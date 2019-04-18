b226ad73fd715112ef6ce3d3817e179ac171b0f8
Toaster <ttoasterrr@gmail.com>
2019-04-18 17:21:29 -0600

    Created

---
+++ Discord Bots/Worked On/Silicon/commands/01.js
@@ -0,0 +1,19 @@
+const discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	name: '',
+	description: '',
+	usage: '',
+	category: '',
+	guildOnly: false,
+	args: false,
+	mod: false,
+	execute(message, content, args, author, authorName, channel, channelName, channelID, guild, guildName, serverPrefix, time, final) {
+		// final.setTitle('')
+		// .setDescription('')
+		// .addField('', '');
+		// message.channel.send(final);
+		// console.log(`[${time}] ${message.author.username} did something.`);
+	},
+};
