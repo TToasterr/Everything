585b3246300dfea4efca26656023f75c25804fb4
Toaster <ttoasterrr@gmail.com>
2019-04-18 19:02:44 -0600

    inviting bot works

---
+++ Discord Bots/Worked On/Silicon/commands/invite.js
@@ -0,0 +1,21 @@
+const discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	name: 'invite',
+	description: 'Invite the bot to your server!',
+	usage: '',
+	category: 'general',
+	passThrough: false,
+	autoExec: false,
+	guildOnly: false,
+	args: false,
+	mod: false,
+	execute(message, content, args, author, authorName, channel, channelName, channelID, guild, guildName, serverPrefix, time, serverSettings, final) {
+		final.setTitle('__**Invite Link**__')
+			.setDescription('Open an invite link by clicking my name above!');
+
+		channel.send(final);
+		console.log(`[${time}] ${authorName} got an invite link.`);
+	},
+};
