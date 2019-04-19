4c6472ff04a9ba0ba38f41783a92d9f8c4cec64e
Toaster <ttoasterrr@gmail.com>
2019-04-18 19:03:09 -0600

    toggling random commands works

---
+++ Discord Bots/Worked On/Silicon/commands/togglerandom.js
@@ -0,0 +1,37 @@
+const discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	name: 'togglerandom',
+	description: 'Toggle on/off the random commands that the community makes!',
+	usage: '',
+	category: 'general',
+	passThrough: false,
+	autoExec: false,
+	guildOnly: true,
+	args: false,
+	mod: true,
+	execute(message, content, args, author, authorName, channel, channelName, channelID, guild, guildName, serverPrefix, time, serverSettings, final) {
+		let state;
+
+		if (serverSettings[`random`]) {
+			state = `off`;
+			serverSettings[`random`] = false;
+		}
+		else {
+			state = `on`;
+			serverSettings[`random`] = true;
+		}
+
+		final.setTitle(`__**Community Commands Toggled!**__`)
+			.setDescription(`Community commands are now ${state}.`);
+
+		serverSettings = JSON.stringify(serverSettings);
+		fs.writeFileSync(`./servers/${guildName}Settings.json`, serverSettings, (err) => {
+			if (err) console.log(`Error writing to '${guildName}'s settings file:\n${err}\n`);
+		});
+
+		channel.send(final);
+		console.log(`[${time}] ${authorName} toggled community commands ${state} for '${guildName}'.`);
+	},
+};
