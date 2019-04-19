85ed6f740c3676b11cf81073c447670a8378df98
Toaster <ttoasterrr@gmail.com>
2019-04-18 19:02:23 -0600

    removing DND channels works

---
+++ Discord Bots/Worked On/Silicon/commands/deldndchannel.js
@@ -0,0 +1,33 @@
+const discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	name: 'deldndchannel',
+	description: 'Remove a DND channel from the server!',
+	usage: '',
+	category: 'dnd',
+	passThrough: true,
+	autoExec: false,
+	guildOnly: true,
+	args: false,
+	mod: false,
+	execute(message, content, args, author, authorName, channel, channelName, channelID, guild, guildName, serverPrefix, time, serverSettings, final) {
+		if (serverSettings[`DNDChannels`][`outOfCharacter`].includes(channelID)) {
+			serverSettings[`DNDChannels`][`outOfCharacter`] = (serverSettings[`DNDChannels`][`outOfCharacter`].join(" ").replace(' ' + channelID, '')).split(" ");
+		}
+		if (serverSettings[`DNDChannels`][`inCharacter`].includes(channelID)) {
+			serverSettings[`DNDChannels`][`inCharacter`] = (serverSettings[`DNDChannels`][`inCharacter`].join(" ").replace(' ' + channelID, '')).split(" ");
+		}
+
+		final.setTitle('__**Channel removed!**__')
+			.setDescription(`<#${channelID}> has been removed from the ${args[0].toLowerCase()} DND channels.`);
+
+		serverSettings = JSON.stringify(serverSettings);
+		fs.writeFileSync(`./servers/${guildName}Settings.json`, serverSettings, (err) => {
+			if (err) console.log(`Error writing to '${guildName}'s settings file:\n${err}\n`);
+		});
+
+		channel.send(final);
+		console.log(`[${time}] ${authorName} removed from the ${args[0].toLowerCase()} DND channels for '${guildName}'.`);
+	},
+};
