4ddc0cb49decf813713ae1af6af07a7f8125472a
Toaster <ttoasterrr@gmail.com>
2019-04-18 19:02:04 -0600

    adding DND channels works

---
+++ Discord Bots/Worked On/Silicon/commands/adddndchannel.js
@@ -0,0 +1,38 @@
+const discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	name: 'adddndchannel',
+	description: 'Add a DND channel to the server!',
+	usage: '[in character or out of character]',
+	category: 'dnd',
+	passThrough: true,
+	autoExec: false,
+	guildOnly: true,
+	args: true,
+	mod: false,
+	execute(message, content, args, author, authorName, channel, channelName, channelID, guild, guildName, serverPrefix, time, serverSettings, final) {
+		args[0] = args[0].slice(1);
+
+		if (args[0] !== `in Character` && args[0] !== `out Of Character`) {
+			final.setTitle(`__**Whoops!**__`)
+				.setDescription(`You didn't use the right arguments!\nPlease use either 'in Character' or 'out Of Character' to set the type of channel.\nThis is case sensitive.`);
+
+			channel.send(final);
+			return console.log(`[${time}] ${authorName} tried to add a DND channel to '${guildName}' but didn't use the right kind of arguments.`);
+		}
+
+		serverSettings[`DNDChannels`][args[0].split(' ').join('')].push(channelID);
+
+		final.setTitle('__**Channel added!**__')
+			.setDescription(`<#${channelID}> has been added to the ${args[0].toLowerCase()} DND channels.`);
+
+		serverSettings = JSON.stringify(serverSettings);
+		fs.writeFileSync(`./servers/${guildName}Settings.json`, serverSettings, (err) => {
+			if (err) console.log(`Error writing to '${guildName}'s settings file:\n${err}\n`);
+		});
+
+		channel.send(final);
+		console.log(`[${time}] ${authorName} added to the ${args[0].toLowerCase()} DND channels for '${guildName}'.`);
+	},
+};
