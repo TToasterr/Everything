b3de08a6d8b50c830a89160180e7d2212820ad48
Toaster <ttoasterrr@gmail.com>
2019-04-22 18:02:05 -0600

    Autoresponder deleting works

---
+++ Discord Bots/Worked On/Silicon/commands/delar.js
@@ -0,0 +1,56 @@
+const discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	name: 'delar',
+	description: 'Remove an autoresponse from this server!',
+	usage: '[trigger]',
+	category: 'autoresponder',
+	passThrough: false,
+	autoExec: false,
+	guildOnly: true,
+	args: true,
+	mod: true,
+	execute(message, content, args, author, authorName, channel, channelName, channelID, guild, guildName, serverPrefix, time, serverSettings, final) {
+		args[0] = args[0].slice(1);
+		let doesntExist = false;
+
+		try {
+			object = fs.readFileSync(`./autoresponders/${guildName}.json`, (err) => {
+				if (err) throw err;
+			});
+
+			object = JSON.parse(object);
+			if (object[args[0]]) {
+				delete object[args[0]];
+			}
+			else {
+				doesntExist = true;
+			}
+		}
+		catch (err) {
+			doesntExist = true;
+		}
+
+
+		if (doesntExist) {
+			final.setTitle(`__**Whoops!**__`)
+				.setDescription(`This server doesnt have that trigger!`);
+
+			channel.send(final);
+			console.log(`[${time}] ${authorName} tried to remove a trigger that doesnt exist from ${guildName}.`);
+		}
+		else {
+			let autoresponses = JSON.stringify(object);
+			fs.writeFileSync(`./autoresponders/${guildName}.json`, autoresponses, (err) => {
+				if (err) console.log(`Error writing to '${guildName}'s autoresponses file:\n${err}\n`);
+			});
+
+			final.setTitle(`__**Autoresponder removed!**__`)
+				.setDescription(`**Trigger:** ${args[0]}\n**Response:** ${object[args[0]]}`);
+
+			channel.send(final);
+			console.log(`[${time}] ${authorName} removed an autoresponder from ${guildName}.`);
+		}
+	},
+};
