cfc2b7a840d3b38e15cc0396d4ada06a1094d501
Toaster <ttoasterrr@gmail.com>
2019-04-22 18:02:37 -0600

    Getting the most used words for the server works

---
+++ Discord Bots/Worked On/Silicon/commands/mostusedwords.js
@@ -0,0 +1,50 @@
+const discord = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	name: 'mostusedwords',
+	description: 'Get the most used words in this server!',
+	usage: '',
+	category: 'random',
+	passThrough: false,
+	autoExec: false,
+	guildOnly: true,
+	args: false,
+	mod: false,
+	execute(message, content, args, author, authorName, channel, channelName, channelID, guild, guildName, serverPrefix, time, serverSettings, final) {
+		let serverwordcount;
+		let wordarray = [];
+		let finalarray = [];
+
+		try {
+			serverwordcount = fs.readFileSync(`./servers/${guildName} wordcounts.json`, (err) => {
+				if (err) throw err;
+			});
+		}
+		catch (err) {
+			serverwordcount = `{}`;
+		}
+		serverwordcount = JSON.parse(serverwordcount);
+
+		let keys = Object.keys(serverwordcount);
+		let array = [];
+		for (let word in serverwordcount) {
+			array.push([word, serverwordcount[word]]);
+		}
+		array.sort(function(a, b) {
+			return a[1] - b[1];
+		});
+
+		wordarray = array.reverse();
+
+		for (var i = 0; i < 10; i++) {
+			finalarray.push(`**${wordarray[i][0]}** - ${wordarray[i][1]}`);
+		}
+
+		final.setTitle(`__**Top 10 most used words on ${guildName}:**__`)
+			.setDescription(finalarray.join(`\n`));
+
+		channel.send(final);
+		console.log(`[${time}] ${authorName} listed the most used words in ${guildName}.`);
+	},
+};
